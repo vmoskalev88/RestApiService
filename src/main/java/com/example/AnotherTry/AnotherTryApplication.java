f1afc7ff7bec7d5a39575a4cdf56c995e989b163
VMoskalev <vladislav.moskalev@gmail.com>
2019-09-13 11:41:40 +0300

    checking 2

--- src/main/java/com/example/AnotherTry/AnotherTryApplication.java
+++ src/main/java/com/example/AnotherTry/AnotherTryApplication.java
@@ -5,9 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AnotherTryApplication {
-
-	// проверка
-
 	public static void main(String[] args) {
 		SpringApplication.run(AnotherTryApplication.class, args);
 	}
